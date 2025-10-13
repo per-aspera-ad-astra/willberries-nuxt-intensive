577edd6cd46497c6bb899d0eb5b51ba3d5c6e9d8
avasylenko13 <vasylenko.a@corestar.com>
2025-10-13 14:27:35 +0300

    Lesson 5

---
+++ app/pages/products.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="long-goods d-block">
+    <div class="container">
+      <div class="row align-items-center mb-4">
+        <div class="col-12">
+          <h2 class="section-title">Category</h2>
+        </div>
+      </div>
+      <div class="row long-goods-list"></div>
+    </div>
+  </section>
+</template>
